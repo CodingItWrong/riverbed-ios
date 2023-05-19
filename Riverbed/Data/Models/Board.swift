8f46e2b321c6969fecb7a46643ee52e67eff4fc1
Josh Justice <me@codingitwrong.com>
2023-05-19 05:12:21 -0700

    Nest JSON:API attributes

--- Riverbed/Data/Models/Board.swift
+++ Riverbed/Data/Models/Board.swift
@@ -3,25 +3,25 @@ import Foundation
 class Board: Codable {
     let type: String
     var id: String
-    var attributes: BoardAttributes
+    var attributes: Board.Attributes
 
-    init(id: String, attributes: BoardAttributes) {
+    init(id: String, attributes: Board.Attributes) {
         self.type = "cards"
         self.id = id
         self.attributes = attributes
     }
-}
 
-class BoardAttributes: Codable {
-    var name: String
-    var icon: String?
-    var colorTheme: String?
-    var favoritedAt: Date?
+    class Attributes: Codable {
+        var name: String
+        var icon: String?
+        var colorTheme: String?
+        var favoritedAt: Date?
 
-    enum CodingKeys: String, CodingKey {
-        case name
-        case icon
-        case colorTheme = "color_theme"
-        case favoritedAt = "favorited_at"
+        enum CodingKeys: String, CodingKey {
+            case name
+            case icon
+            case colorTheme = "color_theme"
+            case favoritedAt = "favorited_at"
+        }
     }
 }
