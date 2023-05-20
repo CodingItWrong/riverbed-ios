39538ec550217c377c2dbd11c69d9806e39a6724
Josh Justice <me@codingitwrong.com>
2023-05-20 06:03:28 -0700

    Disable fields when editing

--- Riverbed/UI/CardViewController.swift
+++ Riverbed/UI/CardViewController.swift
@@ -59,4 +59,9 @@ class ElementTableCell: UITableViewCell {
     @IBOutlet private(set) var elementLabel: UILabel!
     @IBOutlet private(set) var valueTextField: UITextField!
 
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        valueTextField.isEnabled = !editing
+    }
+
 }
