52e00efe3754b7a23a840e271a85242405f9d38e
Josh Justice <me@codingitwrong.com>
2023-05-20 15:36:21 -0700

    Simplify cell lookup code

--- Riverbed/UI/Card/CardViewController.swift
+++ Riverbed/UI/Card/CardViewController.swift
@@ -12,51 +12,26 @@ class CardViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let element = elements[indexPath.row]
         let dataType = element.attributes.dataType ?? .text
-
-        // TODO: try again to extract out the specifying of a type to a variable
-        var cell: ElementCell
-        switch dataType {
-        case .choice:
-            guard let choiceCell = tableView.dequeueReusableCell(
-                withIdentifier: String(describing: ChoiceElementCell.self),
-                for: indexPath) as? ElementCell
-            else { preconditionFailure("Expected a DateElementCell") }
-            cell = choiceCell
-        case .date:
-            guard let dateCell = tableView.dequeueReusableCell(
-                withIdentifier: String(describing: DateElementCell.self),
-                for: indexPath) as? ElementCell
-            else { preconditionFailure("Expected a DateElementCell") }
-            cell = dateCell
-        case .dateTime:
-            guard let dateTimeCell = tableView.dequeueReusableCell(
-                withIdentifier: String(describing: DateElementCell.self),
-                for: indexPath) as? ElementCell
-            else { preconditionFailure("Expected a DateElementCell") }
-            cell = dateTimeCell
-        case .geolocation:
-            guard let geolocationCell = tableView.dequeueReusableCell(
-                withIdentifier: String(describing: GeolocationElementCell.self),
-                for: indexPath) as? ElementCell
-            else { preconditionFailure("Expected a GeolocationElementCell") }
-            cell = geolocationCell
-        case .number:
-            guard let numberCell = tableView.dequeueReusableCell(
-                withIdentifier: String(describing: TextElementCell.self),
-                for: indexPath) as? ElementCell
-            else { preconditionFailure("Expected a TextElementCell") }
-            cell = numberCell
-        case .text:
-            guard let textCell = tableView.dequeueReusableCell(
-                withIdentifier: String(describing: TextElementCell.self),
-                for: indexPath) as? TextElementCell
-            else { preconditionFailure("Expected a TextElementCell") }
-            cell = textCell
-        }
+        let cellType = cellType(for: dataType)
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: String(describing: cellType),
+            for: indexPath) as? ElementCell
+        else { preconditionFailure("Expected a \(String(describing: cellType))") }
         if let card = card {
             cell.update(for: element, and: card)
         }
         return cell
+    }
+
+    private func cellType(for dataType: Element.DataType) -> UITableViewCell.Type {
+        switch dataType {
+        case .choice: return ChoiceElementCell.self
+        case .date: return DateElementCell.self
+        case .dateTime: return DateElementCell.self
+        case .geolocation: return GeolocationElementCell.self
+        case .number: return TextElementCell.self
+        case .text: return TextElementCell.self
+        }
     }
 
     @IBAction func toggleEditing(_ sender: UIButton) {
