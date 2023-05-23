9a0165679195cde62930e0c0647df52e7e6a172a
Josh Justice <me@codingitwrong.com>
2023-05-23 06:04:15 -0400

    Move grouping logic out of VC

--- Riverbed/Data/Models/Card.swift
+++ Riverbed/Data/Models/Card.swift
@@ -18,7 +18,128 @@ class Card: NSObject, Codable {
             case fieldValues = "field-values"
         }
     }
+}
 
+extension Card {
+    static func group(cards: [Card], for column: Column, with elements: [Element]) -> [CardGroup] {
+        // filter
+        var filteredCards: [Card]
+        if let cardInclusionConditions = column.attributes.cardInclusionConditions {
+            filteredCards = cards.filter { (card) in
+                checkConditions(fieldValues: card.attributes.fieldValues,
+                                conditions: cardInclusionConditions,
+                                elements: elements)
+            }
+        } else {
+            filteredCards = cards
+        }
+
+        // TODO: are choice labels used for choice fields?
+
+        // sort
+        var sortedCards: [Card]
+        if let cardSortOrder = column.attributes.cardSortOrder {
+            sortedCards = sort(cards: filteredCards, by: cardSortOrder)
+        } else {
+            sortedCards = filteredCards
+        }
+
+        // group
+        let cardGrouping = column.attributes.cardGrouping
+        return group(cards: sortedCards, by: cardGrouping, elements: elements)
+    }
+
+    private static func checkConditions(fieldValues: [String: FieldValue?],
+                                        conditions: [Condition]?,
+                                        elements: [Element]) -> Bool {
+        guard let conditions = conditions else {
+            return true
+        }
+
+        return conditions.allSatisfy { (condition) in
+            guard let field = condition.field,
+                  let query = condition.query,
+                  field != "" else {
+                return true
+            }
+
+            let fieldObject = elements.first { $0.id == field }
+            guard let dataType = fieldObject?.attributes.dataType else {
+                return true
+            }
+            var value: FieldValue?
+            if let tempValue = fieldValues[field] {
+                value = tempValue // attempt to handle a double optional
+            }
+            return query.match(value: value, dataType: dataType, options: condition.options)
+        }
+    }
+
+    private static func sort(cards: [Card],
+                             by sortOrder: Column.SortOrder) -> [Card] {
+        guard let field = sortOrder.field,
+              let direction = sortOrder.direction else {
+            return cards
+        }
+
+        let sortedCards = cards.sorted { (cardA, cardB) in
+            guard let aValue = cardA.attributes.fieldValues[field],
+                  case let .string(aValue) = aValue else { return true }
+            guard let bValue = cardB.attributes.fieldValues[field],
+                  case let .string(bValue) = bValue else { return false }
+            return aValue < bValue
+        }
+        switch direction {
+        case .ascending: return sortedCards
+        case .descending: return sortedCards.reversed()
+        }
+    }
+
+    private static func group(cards: [Card],
+                              by cardGrouping: Column.SortOrder?,
+                              elements: [Element]) -> [CardGroup] {
+        guard let cardGrouping = cardGrouping,
+              let field = cardGrouping.field,
+              let direction = cardGrouping.direction,
+              let groupField = elements.first(where: { $0.id == field }) else {
+            if cards.isEmpty {
+                return []
+            } else {
+                return [CardGroup(value: nil, cards: cards)]
+            }
+        }
+
+        var cardGroups = [CardGroup]()
+
+        cards.forEach { (card) in
+            let groupValue: FieldValue? = {
+                if let groupValue = card.attributes.fieldValues[field] {
+                    return groupValue
+                } else {
+                    return nil
+                }
+            }()
+
+            if let existingGroup = cardGroups.first(where: { $0.value == groupValue }) {
+                existingGroup.cards.append(card)
+            } else {
+                let newGroup = CardGroup(value: groupValue, cards: [card])
+                cardGroups.append(newGroup)
+            }
+        }
+
+        let sortedCardGroups = cardGroups.sorted { (lhs, rhs) in
+            guard let aValue = groupField.sortValue(from: lhs.value) else { return true }
+            guard let bValue = groupField.sortValue(from: rhs.value) else { return false }
+
+            return aValue > bValue
+        }
+
+        switch direction {
+        case .ascending: return sortedCardGroups
+        case .descending: return sortedCardGroups.reversed()
+        }
+    }
 }
 
 enum FieldValue: Codable, Equatable {
