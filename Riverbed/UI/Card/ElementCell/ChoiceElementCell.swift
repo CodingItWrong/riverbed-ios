dc6b9efb7fbcf5a9354f9730dcc950fbe1da9232
Josh Justice <me@codingitwrong.com>
2023-05-26 06:41:46 -0400

    Save choices

--- Riverbed/UI/Card/ElementCell/ChoiceElementCell.swift
+++ Riverbed/UI/Card/ElementCell/ChoiceElementCell.swift
@@ -4,15 +4,19 @@ class ChoiceElementCell: UITableViewCell, ElementCell {
 
     weak var delegate: ElementCellDelegate?
 
+    private var element: Element?
+
     @IBOutlet private(set) var elementLabel: UILabel!
     @IBOutlet private(set) var valuePopUpButton: UIButton!
 
     func update(for element: Element, and card: Card) {
+        self.element = element
+
         elementLabel.text = element.attributes.name
         let choices = element.attributes.options?.choices ?? []
         let menuOptions = choices.map { (choice) in
-            UIAction(title: choice.label) { _ in
-                print("Tapped \(choice.id)")
+            UIAction(title: choice.label) { [weak self] _ in
+                self?.passUpdatedValueToDelegate(choice)
             }
         }
         valuePopUpButton.menu = UIMenu(children: menuOptions)
@@ -21,5 +25,10 @@ class ChoiceElementCell: UITableViewCell, ElementCell {
     override func setEditing(_ editing: Bool, animated: Bool) {
         super.setEditing(editing, animated: animated)
         valuePopUpButton.isEnabled = !editing
+    }
+
+    func passUpdatedValueToDelegate(_ choice: Element.Choice) {
+        guard let element = element else { return }
+        delegate?.update(value: .string(choice.id), for: element)
     }
 }
