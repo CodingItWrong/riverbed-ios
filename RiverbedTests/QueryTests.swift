7964acfa8eb348efe83a8f9c3b163fb8ec4485b3
Josh Justice <me@codingitwrong.com>
2023-06-02 07:12:23 -0400

    Simplify test cases

--- RiverbedTests/QueryTests.swift
+++ RiverbedTests/QueryTests.swift
@@ -2,80 +2,44 @@
 import XCTest
 
 final class QueryTests: XCTestCase {
-    var dataType: Element.DataType!
-    var options: Condition.Options!
-    var value: FieldValue?
-
-    override func setUp() {
-        super.setUp()
-        dataType = .text // for cases where the type doesn't matter
-        options = Condition.Options(value: nil) // for cases where the options don't matter
-    }
-
-    override func tearDown() {
-        super.tearDown()
-        dataType = nil
-        options = nil
-        value = nil
-    }
-
     // MARK: - does not equal
 
     func test_match_doesNotEqual_bothNil_doesNotMatch() {
-        options = Condition.Options(value: nil)
-        value = nil
-
-        let isMatch = Query.doesNotEqual.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.doesNotEqual.match(value: nil,
+                                               dataType: .text,
+                                               options: Condition.Options(value: nil))
 
         XCTAssertFalse(isMatch)
     }
 
     func test_match_doesNotEqual_onlyOptionNil_matches() {
-        dataType = .text
-        options = Condition.Options(value: nil)
-        let value: FieldValue? = .string("hello")
-
-        let isMatch = Query.doesNotEqual.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.doesNotEqual.match(value: .string("hello"),
+                                               dataType: .text,
+                                               options: Condition.Options(value: nil))
 
         XCTAssertTrue(isMatch)
     }
 
     func test_match_doesNotEqual_onlyValueNil_matches() {
-        dataType = .text
-        options = Condition.Options(value: "hello")
-        let value: FieldValue? = nil
-
-        let isMatch = Query.doesNotEqual.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.doesNotEqual.match(value: nil,
+                                               dataType: .text,
+                                               options: Condition.Options(value: "hello"))
 
         XCTAssertTrue(isMatch)
     }
 
     func test_match_doesNotEqual_differentStrings_matches() {
-        dataType = .text
-        options = Condition.Options(value: "a")
-        let value: FieldValue? = .string("b")
-
-        let isMatch = Query.doesNotEqual.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.doesNotEqual.match(value: .string("a"),
+                                               dataType: .text,
+                                               options: Condition.Options(value: "b"))
 
         XCTAssertTrue(isMatch)
     }
 
-    func test_match_doesNotEqual_sameString_doesNotMatch() {
-        dataType = .text
-        options = Condition.Options(value: "a")
-        let value: FieldValue? = .string("a")
-
-        let isMatch = Query.doesNotEqual.match(value: value, dataType: dataType, options: options)
-
-        XCTAssertFalse(isMatch)
-    }
-
     func test_match_doesNotEqual_differentCase_matches() {
-        dataType = .text
-        options = Condition.Options(value: "a")
-        let value: FieldValue? = .string("A")
-
-        let isMatch = Query.doesNotEqual.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.doesNotEqual.match(value: .string("a"),
+                                               dataType: .text,
+                                               options: Condition.Options(value: "A"))
 
         XCTAssertTrue(isMatch)
     }
@@ -83,60 +47,49 @@ final class QueryTests: XCTestCase {
     // MARK: - equals
 
     func test_match_equals_bothNil_matches() {
-        options = Condition.Options(value: nil)
-        value = nil
-
-        let isMatch = Query.equals.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.equals.match(value: nil,
+                                         dataType: .text,
+                                         options: Condition.Options(value: nil))
 
         XCTAssertTrue(isMatch)
     }
 
     func test_match_equals_onlyOptionNil_doesNotMatch() {
-        dataType = .text
-        options = Condition.Options(value: nil)
-        let value: FieldValue? = .string("hello")
-
-        let isMatch = Query.equals.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.equals.match(value: .string("hello"),
+                                         dataType: .text,
+                                         options: Condition.Options(value: nil))
 
         XCTAssertFalse(isMatch)
     }
 
     func test_match_equals_onlyValueNil_doesNotMatch() {
-        dataType = .text
-        options = Condition.Options(value: "hello")
-        let value: FieldValue? = nil
-
-        let isMatch = Query.equals.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.equals.match(value: nil,
+                                         dataType: .text,
+                                         options: Condition.Options(value: "hello"))
 
         XCTAssertFalse(isMatch)
     }
 
     func test_match_equals_differentStrings_doesNotMatch() {
-        dataType = .text
-        options = Condition.Options(value: "a")
-        let value: FieldValue? = .string("b")
-
-        let isMatch = Query.equals.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.equals.match(value: .string("a"),
+                                         dataType: .text,
+                                         options: Condition.Options(value: "b"))
 
         XCTAssertFalse(isMatch)
     }
 
     func test_match_equals_sameString_matches() {
-        dataType = .text
-        options = Condition.Options(value: "a")
-        let value: FieldValue? = .string("a")
-
-        let isMatch = Query.equals.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.equals.match(value: .string("a"),
+                                         dataType: .text,
+                                         options: Condition.Options(value: "a"))
 
         XCTAssertTrue(isMatch)
     }
 
     func test_match_equals_differentCase_doesNotMatch() {
-        dataType = .text
-        options = Condition.Options(value: "a")
-        let value: FieldValue? = .string("A")
-
-        let isMatch = Query.equals.match(value: value, dataType: dataType, options: options)
+        let isMatch = Query.equals.match(value: .string("a"),
+                                         dataType: .text,
+                                         options: Condition.Options(value: "A"))
 
         XCTAssertFalse(isMatch)
     }
