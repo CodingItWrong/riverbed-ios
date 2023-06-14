309aa58b1bed2b53f2e98d6469b46c7894806d01
Josh Justice <me@codingitwrong.com>
2023-06-14 06:41:56 -0400

    Show read-only field in edit mode

--- Riverbed/UI/ElementCells/ReadOnlyElementCell.swift
+++ Riverbed/UI/ElementCells/ReadOnlyElementCell.swift
@@ -9,6 +9,8 @@ class ReadOnlyElementCell: UITableViewCell, ElementCell {
     func update(for element: Element, allElements: [Element], fieldValue: FieldValue?) {
         if let value = fieldValue {
             valueLabel.text = element.formatString(from: value)
+        } else if isEditing {
+            valueLabel.text = element.attributes.name ?? "(unnamed field)"
         } else {
             valueLabel.text = ""
         }
