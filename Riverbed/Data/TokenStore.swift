71af0d39599b600771ec79d5aa46c0e6dbb40a7a
Josh Justice <me@codingitwrong.com>
2023-06-17 07:02:53 -0400

    Remove accidentally copied methods

--- Riverbed/Data/TokenStore.swift
+++ Riverbed/Data/TokenStore.swift
@@ -63,47 +63,4 @@ class TokenStore: BaseStore {
             completion(.failure(error))
         }
     }
-
-    func update(_ card: Card,
-                with fieldValues: [String: FieldValue?],
-                completion: @escaping (Result<Void, Error>) -> Void) {
-        let url = RiverbedAPI.cardURL(for: card)
-        var request = URLRequest(url: url)
-        request.httpMethod = "PATCH"
-        request.setValue("application/vnd.api+json", forHTTPHeaderField: "Content-Type")
-        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
-
-        let updatedCard = Card(id: card.id, attributes: Card.Attributes(fieldValues: fieldValues))
-
-        do {
-            let encoder = JSONEncoder()
-            let requestBody = try encoder.encode(RiverbedAPI.RequestBody(data: updatedCard))
-            request.httpBody = requestBody
-
-            let task = session.dataTask(with: request) { (data, response, error) in
-                let result: Result<Void, Error> = self.processVoidResult((data, response, error))
-                OperationQueue.main.addOperation {
-                    completion(result)
-                }
-            }
-            task.resume()
-        } catch {
-            completion(.failure(error))
-        }
-    }
-
-    func delete(_ card: Card, completion: @escaping (Result<Void, Error>) -> Void) {
-        let url = RiverbedAPI.cardURL(for: card)
-        var request = URLRequest(url: url)
-        request.httpMethod = "DELETE"
-        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
-
-        let task = session.dataTask(with: request) { (data, response, error) in
-            let result: Result<Void, Error> = self.processVoidResult((data, response, error))
-            OperationQueue.main.addOperation {
-                completion(result)
-            }
-        }
-        task.resume()
-    }
 }
