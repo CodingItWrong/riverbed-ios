455046e8d40dc7659457157777ad7a66d33e0b94
Josh Justice <me@codingitwrong.com>
2023-06-28 14:17:10 -0600

    Add action tests

---
+++ RiverbedTests/ActionTests.swift
@@ -0,0 +1,50 @@
+@testable import Riverbed
+import XCTest
+
+final class ActionTests: XCTestCase {
+    func test_call_withEmptyCommand_returnsFieldValuesUnchanged() {
+        let fieldValues: [String: FieldValue?] = ["A": .string("B")]
+        let action = Action(command: nil, field: "A", value: "anything")
+        let result = action.call(elements: [], fieldValues: fieldValues)
+        XCTAssertEqual(result, fieldValues)
+    }
+
+    func test_call_configuredToSetValueToEmpty_returnsFieldValuesWithValueSetToNil() {
+        let elements = [
+            Element(id: "A", attributes: Element.Attributes(
+                elementType: .field,
+                dataType: .text,
+                options: Element.Options()))
+        ]
+        let fieldValues: [String: FieldValue?] = ["A": .string("B")]
+        let action = Action(command: .setValue, field: "A", value: Value.empty.rawValue)
+        let result = action.call(elements: elements, fieldValues: fieldValues)
+        XCTAssertEqual(result, ["A": nil])
+    }
+
+    func test_call_configuredToAdd2DaysToFutureDate_returnsFieldValuesWithDate2DaysLater() {
+        let elements = [
+            Element(id: "A", attributes: Element.Attributes(
+                elementType: .field,
+                dataType: .date,
+                options: Element.Options()))
+        ]
+        let fieldValues: [String: FieldValue?] = ["A": .string("2999-01-01")]
+        let action = Action(command: .addDays, field: "A", value: "2")
+        let result = action.call(elements: elements, fieldValues: fieldValues)
+        XCTAssertEqual(result, ["A": .string("2999-01-03")])
+    }
+
+    func test_call_configuredToAddNegative1DaysToFutureDatetime_returnsFieldValuesWithDatetime1DayEarlier() {
+        let elements = [
+            Element(id: "A", attributes: Element.Attributes(
+                elementType: .field,
+                dataType: .dateTime,
+                options: Element.Options()))
+        ]
+        let fieldValues: [String: FieldValue?] = ["A": .string("2999-01-10T09:00:00.000Z")]
+        let action = Action(command: .addDays, field: "A", value: "-1")
+        let result = action.call(elements: elements, fieldValues: fieldValues)
+        XCTAssertEqual(result, ["A": .string("2999-01-09T09:00:00.000Z")])
+    }
+}
