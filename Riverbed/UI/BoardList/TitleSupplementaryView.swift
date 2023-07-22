14ecd351edf1919edc1d00b83dee076ee52b562c
Josh Justice <me@codingitwrong.com>
2023-07-22 06:08:24 -0400

    Adjust alignment of text in column

--- Riverbed/UI/BoardList/TitleSupplementaryView.swift
+++ Riverbed/UI/BoardList/TitleSupplementaryView.swift
@@ -18,10 +18,10 @@ extension TitleSupplementaryView {
         addSubview(label)
         label.translatesAutoresizingMaskIntoConstraints = false
         label.adjustsFontForContentSizeCategory = true
-        let inset = CGFloat(10)
+        let horizontalInset = CGFloat(16)
         NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: inset),
-            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -inset),
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: horizontalInset),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -horizontalInset),
             label.topAnchor.constraint(equalTo: topAnchor, constant: 0),
             label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -0)
         ])
