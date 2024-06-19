ac719a30571348720f369cd198eb3e3dc27f54b6
Josh Justice <me@codingitwrong.com>
2024-06-19 13:05:00 -0500

    Test remainder setter cases

--- RiverbedTests/EditBoardViewControllerTests.swift
+++ RiverbedTests/EditBoardViewControllerTests.swift
@@ -3,11 +3,33 @@ import XCTest
 
 final class EditBoardViewControllerTests: XCTestCase {
     
+    private var attributes: Board.Attributes!
+    private var urlField: Element!
+    private var titleField: Element!
     private var sut: EditBoardViewController!
     
     override func setUp() {
         super.setUp()
         sut = EditBoardViewController()
+        
+        urlField = Element(id: "url_field_id", attributes: Element.Attributes(elementType: .field, name: "URL"))
+        titleField = Element(id: "title_field_id", attributes: Element.Attributes(elementType: .field, name: "Title"))
+        sut.fields = [urlField, titleField]
+
+        let webhooks = Board.Webhooks()
+        webhooks.cardCreate = "original card create URL"
+        webhooks.cardUpdate = "original card update URL"
+        let share = Board.Share()
+        share.urlField = urlField.id
+        share.titleField = titleField.id
+        let options = Board.Options()
+        options.webhooks = webhooks
+        options.share = share
+        attributes = Board.Attributes(name: "original board name",
+                                      colorTheme: .red,
+                                      options: options)
+        sut.attributes = attributes
+        
         sut.loadViewIfNeeded()
     }
     
@@ -21,24 +43,18 @@ final class EditBoardViewControllerTests: XCTestCase {
     }
     
     func test_cellForRowAt_withRow0_shouldConfigureBoardNameCell() {
-        let boardName = "Fake Board Name"
-        sut.attributes = Board.Attributes(name: boardName)
-
         let cell = sut.tableView(sut.tableView, cellForRowAt: IndexPath(row: 0, section: 0)) as! TextFieldCell
         
         XCTAssertEqual(cell.label.text, "Board Name")
-        XCTAssertEqual(cell.textField.text, boardName)
+        XCTAssertEqual(cell.textField.text, attributes.name)
         XCTAssertNotNil(cell.textField.delegate) // can't test for equality with sut for some reason
     }
     
     func test_cellForRowAt_withRow1_shouldConfigureColorThemeCell() {
-        let colorTheme = ColorTheme.pink
-        sut.attributes = Board.Attributes(colorTheme: colorTheme)
-
         let cell = sut.tableView(sut.tableView, cellForRowAt: IndexPath(row: 1, section: 0)) as! PopUpButtonCell
         
         XCTAssertEqual(cell.label.text, "Color Theme")
-        XCTAssertEqual(cell.selectedValue as! ColorTheme, colorTheme)
+        XCTAssertEqual(cell.selectedValue as? ColorTheme, attributes.colorTheme)
         XCTAssertEqual(cell.popUpButton.menu?.children.count, 9)
         XCTAssertEqual(cell.popUpButton.menu?.children[0].title, "Default")
         XCTAssertEqual(cell.popUpButton.menu?.children[1].title, "Red")
@@ -52,60 +68,34 @@ final class EditBoardViewControllerTests: XCTestCase {
     }
     
     func test_cellForRowAt_withRow2_shouldConfigureIconCell() {
-        let icon = Icon.book
-        sut.attributes = Board.Attributes(iconName: icon.rawValue)
-
         let cell = sut.tableView(sut.tableView, cellForRowAt: IndexPath(row: 2, section: 0)) as! PopUpButtonCell
         
         XCTAssertEqual(cell.label.text, "Icon")
-        XCTAssertEqual(cell.selectedValue as! Icon, icon)
+        XCTAssertEqual(cell.selectedValue as? Icon, attributes.icon)
         XCTAssertEqual(cell.popUpButton.menu?.children.count, 16)
         XCTAssertEqual(cell.popUpButton.menu?.children[0].title, "(none)")
         XCTAssertEqual(cell.popUpButton.menu?.children[1].title, "Baseball")
     }
     
     func test_cellForRowAt_withRow3_shouldConfigureCardCreateWebhookCell() {
-        // TODO: simplify initalization
-        let webhooks = Board.Webhooks()
-        webhooks.cardCreate = "fake card create URL"
-        let options = Board.Options()
-        options.webhooks = webhooks
-        sut.attributes = Board.Attributes(options: options)
-
         let cell = sut.tableView(sut.tableView, cellForRowAt: IndexPath(row: 3, section: 0)) as! TextFieldCell
         
         XCTAssertEqual(cell.label.text, "Card Create Webhook")
-        XCTAssertEqual(cell.textField.text, webhooks.cardCreate)
+        XCTAssertEqual(cell.textField.text, attributes.options?.webhooks?.cardCreate)
     }
     
     func test_cellForRowAt_withRow4_shouldConfigureCardUpdateWebhookCell() {
-        // TODO: simplify initalization
-        let webhooks = Board.Webhooks()
-        webhooks.cardUpdate = "fake card update URL"
-        let options = Board.Options()
-        options.webhooks = webhooks
-        sut.attributes = Board.Attributes(options: options)
-
         let cell = sut.tableView(sut.tableView, cellForRowAt: IndexPath(row: 4, section: 0)) as! TextFieldCell
         
         XCTAssertEqual(cell.label.text, "Card Update Webhook")
-        XCTAssertEqual(cell.textField.text, webhooks.cardUpdate)
+        XCTAssertEqual(cell.textField.text, attributes.options?.webhooks?.cardUpdate)
     }
     
     func test_cellForRowAt_withRow5_shouldConfigureShareUrlCell() {
-        let urlField = Element(id: "url_field_id", attributes: Element.Attributes(elementType: .field, name: "URL"))
-        let titleField = Element(id: "title_field_id", attributes: Element.Attributes(elementType: .field, name: "Title"))
-        sut.fields = [urlField, titleField]
-        let share = Board.Share()
-        share.urlField = urlField.id
-        let options = Board.Options()
-        options.share = share
-        sut.attributes = Board.Attributes(options: options)
-
         let cell = sut.tableView(sut.tableView, cellForRowAt: IndexPath(row: 5, section: 0)) as! PopUpButtonCell
         
         XCTAssertEqual(cell.label.text, "Share URL Field")
-        XCTAssertEqual(cell.selectedValue as! Element, urlField)
+        XCTAssertEqual(cell.selectedValue as? Element, urlField)
         XCTAssertEqual(cell.popUpButton.menu?.children.count, 3)
         XCTAssertEqual(cell.popUpButton.menu?.children[0].title, "(none)")
         XCTAssertEqual(cell.popUpButton.menu?.children[1].title, "URL")
@@ -113,19 +103,10 @@ final class EditBoardViewControllerTests: XCTestCase {
     }
 
     func test_cellForRowAt_withRow6_shouldConfigureShareTitleCell() {
-        let urlField = Element(id: "url_field_id", attributes: Element.Attributes(elementType: .field, name: "URL"))
-        let titleField = Element(id: "title_field_id", attributes: Element.Attributes(elementType: .field, name: "Title"))
-        sut.fields = [urlField, titleField]
-        let share = Board.Share()
-        share.titleField = titleField.id
-        let options = Board.Options()
-        options.share = share
-        sut.attributes = Board.Attributes(options: options)
-
         let cell = sut.tableView(sut.tableView, cellForRowAt: IndexPath(row: 6, section: 0)) as! PopUpButtonCell
         
         XCTAssertEqual(cell.label.text, "Share Title Field")
-        XCTAssertEqual(cell.selectedValue as! Element, titleField)
+        XCTAssertEqual(cell.selectedValue as? Element, titleField)
         XCTAssertEqual(cell.popUpButton.menu?.children.count, 3)
         XCTAssertEqual(cell.popUpButton.menu?.children[0].title, "(none)")
         XCTAssertEqual(cell.popUpButton.menu?.children[1].title, "URL")
@@ -133,7 +114,6 @@ final class EditBoardViewControllerTests: XCTestCase {
     }
     
     func test_valueDidChangeInFormCell_withRow0_shouldSetBoardName() {
-        sut.attributes = Board.Attributes(name: "original board name")
         let updatedBoardName = "updated board name"
         
         let indexPath = IndexPath(row: 0, section: 0)
@@ -146,12 +126,13 @@ final class EditBoardViewControllerTests: XCTestCase {
     }
     
     func test_valueDidChangeInFormCell_withRow1_shouldSetColorTheme() {
-        sut.attributes = Board.Attributes(colorTheme: .red)
         let updatedColorTheme = ColorTheme.green
         
         let indexPath = IndexPath(row: 1, section: 0)
         let cell = sut.tableView(sut.tableView, cellForRowAt: indexPath) as! PopUpButtonCell
 
+        XCTAssertNotEqual(sut.attributes.colorTheme, updatedColorTheme) // precondition
+        
         cell.selectedValue = updatedColorTheme
         sut.valueDidChange(inFormCell: cell, at: indexPath)
         
@@ -159,12 +140,13 @@ final class EditBoardViewControllerTests: XCTestCase {
     }
     
     func test_valueDidChangeInFormCell_withRow2_shouldSetSetIcon() {
-        sut.attributes = Board.Attributes(iconName: Icon.money.rawValue)
         let updatedIcon = Icon.food
         
         let indexPath = IndexPath(row: 2, section: 0)
         let cell = sut.tableView(sut.tableView, cellForRowAt: indexPath) as! PopUpButtonCell
 
+        XCTAssertNotEqual(sut.attributes.icon, updatedIcon) // precondition
+        
         cell.selectedValue = updatedIcon
         sut.valueDidChange(inFormCell: cell, at: indexPath)
         
@@ -172,12 +154,6 @@ final class EditBoardViewControllerTests: XCTestCase {
     }
     
     func test_valueDidChangeInFormCell_withRow3_shouldSetCardCreateWebhook() {
-        let webhooks = Board.Webhooks()
-        webhooks.cardCreate = "original card create URL"
-        let options = Board.Options()
-        options.webhooks = webhooks
-        sut.attributes = Board.Attributes(options: options)
-        
         let updatedCardCreateWebhook = "updated card create URL"
         
         let indexPath = IndexPath(row: 3, section: 0)
@@ -187,5 +163,41 @@ final class EditBoardViewControllerTests: XCTestCase {
         sut.valueDidChange(inFormCell: cell, at: indexPath)
 
         XCTAssertEqual(sut.attributes.options?.webhooks?.cardCreate, updatedCardCreateWebhook)
+    }
+    
+    func test_valueDidChangeInFormCell_withRow4_shouldSetCardUpdateWebhook() {
+        let updatedCardUpdateWebhook = "updated card update URL"
+        
+        let indexPath = IndexPath(row: 4, section: 0)
+        let cell = sut.tableView(sut.tableView, cellForRowAt: indexPath) as! TextFieldCell
+        
+        cell.textField.text = updatedCardUpdateWebhook
+        sut.valueDidChange(inFormCell: cell, at: indexPath)
+
+        XCTAssertEqual(sut.attributes.options?.webhooks?.cardUpdate, updatedCardUpdateWebhook)
+    }
+    
+    func test_valueDidChangeInFormCell_withRow5_shouldSetShareUrlField() {
+        let updatedShareUrlField = titleField
+        
+        let indexPath = IndexPath(row: 5, section: 0)
+        let cell = sut.tableView(sut.tableView, cellForRowAt: indexPath) as! PopUpButtonCell
+
+        cell.selectedValue = updatedShareUrlField
+        sut.valueDidChange(inFormCell: cell, at: indexPath)
+        
+        XCTAssertEqual(sut.attributes.options?.share?.urlField, updatedShareUrlField?.id)
+    }
+    
+    func test_valueDidChangeInFormCell_withRow6_shouldSetShareTitleField() {
+        let updatedShareTitleField = urlField
+        
+        let indexPath = IndexPath(row: 6, section: 0)
+        let cell = sut.tableView(sut.tableView, cellForRowAt: indexPath) as! PopUpButtonCell
+
+        cell.selectedValue = updatedShareTitleField
+        sut.valueDidChange(inFormCell: cell, at: indexPath)
+        
+        XCTAssertEqual(sut.attributes.options?.share?.urlField, updatedShareTitleField?.id)
     }
 }
