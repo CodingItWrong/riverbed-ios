811c733eedbf9bc2720df31597255dc63193c40d
Josh Justice <me@codingitwrong.com>
2025-06-14 14:40:17 -0400

    Rounded corners on built in text fields

--- Riverbed/UI/Form/TextFieldCell.swift
+++ Riverbed/UI/Form/TextFieldCell.swift
@@ -5,7 +5,13 @@ class TextFieldCell: UITableViewCell, UITextFieldDelegate {
     weak var delegate: FormCellDelegate?
 
     @IBOutlet var label: UILabel!
-    @IBOutlet var textField: UITextField!
+    @IBOutlet var textField: UITextField! {
+        didSet {
+            textField.layer.cornerRadius = 5
+            textField.layer.borderWidth = 1
+            textField.layer.borderColor = UIColor.separator.cgColor
+        }
+    }
 
     func textFieldDidChangeSelection(_ textField: UITextField) {
         delegate?.valueDidChange(inFormCell: self)
