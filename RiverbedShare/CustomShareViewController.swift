6e556620e1a6f94fa4c0f2ad5bb72c99ee50242e
Josh Justice <me@codingitwrong.com>
2025-07-13 07:30:13 -0500

    Set up UI to customize url and title

--- RiverbedShare/CustomShareViewController.swift
+++ RiverbedShare/CustomShareViewController.swift
@@ -5,40 +5,67 @@ class CustomShareViewController: UIViewController {
     private let attachmentHandler = AttachmentHandler()
     private let webhookStore = WebhookStore()
     
+    @IBOutlet private(set) var urlField: UITextField!
+    @IBOutlet private(set) var titleField: UITextField!
+    @IBOutlet private(set) var saveButton: UIButton!
+    @IBOutlet private(set) var cancelButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("CustomShareViewController viewDidLoad()")
-        
-        guard let context = extensionContext,
-            let items = context.inputItems as? [NSExtensionItem],
-            let item = items.first,
-            let attachments = item.attachments else {
-                alert(message: "An error occurred: URL not found", completion: self.done)
-                return
+        if #available(iOS 26, *) {
+            saveButton.configuration = .prominentGlass()
+            saveButton.setImage(UIImage(systemName: "checkmark"), for: .normal)
+            
+            cancelButton.configuration = .glass()
+            cancelButton.setImage(UIImage(systemName: "xmark"), for: .normal)
         }
-
+        
+        prepopulateTextFields()
+    }
+                
+    @IBAction func save() {
+        guard let url = urlField.text,
+              let title = titleField.text else {
+            fatalError("unreachable")
+        }
+        
+        let bodyDict = ["url": url, "title": title]
+        
+        webhookStore.postWebhook(bodyDict: bodyDict) { result in
+            switch result {
+            case .success:
+                self.alert(
+                    message: "Link saved.",
+                    completion: self.done)
+            case .failure(let error):
+                self.alert(
+                    message: error.localizedDescription,
+                    completion: self.done)
+            }
+        }
+    }
+    
+    @IBAction func cancel() {
+        self.done()
+    }
+    
+    // MARK: - helper methods
+    
+    private func prepopulateTextFields() {
+        guard let context = extensionContext,
+              let items = context.inputItems as? [NSExtensionItem],
+              let item = items.first,
+              let attachments = item.attachments else {
+            alert(message: "An error occurred: URL not found", completion: self.done)
+            return
+        }
+        
         attachmentHandler.getURL(attachments: attachments) { [self] result in
             switch result {
             case .success(let sharedURL):
-                let bodyDict = [
-                    "url": sharedURL.absoluteString,
-//                    "title": self.contentText // TODO: prompt user
-                    ]
-                
-                // TEMP: jump straight to posting webhook, without prompting user for title
-                webhookStore.postWebhook(bodyDict: bodyDict) { result in
-                    switch result {
-                    case .success:
-                        self.alert(
-                            message: "Link saved.",
-                            completion: self.done)
-                    case .failure(let error):
-                        self.alert(
-                            message: error.localizedDescription,
-                            completion: self.done)
-                    }
-                }
+                self.urlField.text = sharedURL.absoluteString
+                // not pre-setting title currently
             case .failure(let error):
                 self.alert(
                     message: "An error occurred: \(error.localizedDescription)",
